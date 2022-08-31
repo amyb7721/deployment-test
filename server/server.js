853fb6a1af71614a2d442eccacee08b103408581
amyb7721 <amyb7721@gmail.com>
2022-08-31 13:30:40 -0500

    updated

--- server/server.js
+++ server/server.js
@@ -12,7 +12,8 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "../client/index.html"))
 })
 
-const port = 4005;
+// we would use port 4005 if herkuo cant find it. we are connecting it here.
+const port = process.env.PORT || 4005;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
